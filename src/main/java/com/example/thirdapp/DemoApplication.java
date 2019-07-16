5b62381502e0ee34628379578d97c8a9b9037723
MohanKumarHG <mohan.kumar@danaherdigital.com>
2019-07-16 06:59:01 +0000

    added comments to main app

--- src/main/java/com/example/thirdapp/DemoApplication.java
+++ src/main/java/com/example/thirdapp/DemoApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+//this is a commented line	
+public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
